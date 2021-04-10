ebdb7be31f1673c1d1e257317d7b64aaf4c3423e
wei.zhang <wei.zhang@modooplay.com>
2021-04-10 10:59:48 +0800

    Init

---
+++ URPSourceCodeLearn/Assets/CustomPipelineAsset.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        throw new System.NotImplementedException();
+    }
+}
